c388bc545ee735cc5e9695c58cce92a763397d12
tauqeerrrrr <royalsaikh650@gmail.com>
2024-10-25 22:21:14 -0700

    Add files via upload

---
+++ linklist.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+class node
+{
+    int data;
+    node* next;
+
+};
+int maim()
+ { int node* node1 new node();
+cout<<node1->data<<node1->next<<endl;
+
+}
